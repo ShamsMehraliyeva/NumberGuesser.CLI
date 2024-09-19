28c8fe649578bcd24249bba0f8ba1d563515333b
Shams Mehraliyeva <shams.mehralieva99@gmail.com>
2024-09-19 16:31:29 +0400

    add AdvancedGameSettings

--- NumberGuesser.CLI/GameSettings.cs
+++ NumberGuesser.CLI/GameSettings.cs
@@ -1,3 +1,4 @@
 namespace NumberGuesser.CLI;
 
 public record GameSettings(int LowerBound, int UpperBound, int MaxAttempts);
+public record AdvancedGameSettings(int LowerBound, int UpperBound, int MaxAttempts, int DifficultyLevel) : GameSettings(LowerBound, UpperBound, MaxAttempts);
